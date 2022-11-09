d39fcb83b7fba7dbb3e031a2d850e6d008e8e8a2
Eduardo <eduardocorreaaranda10@gmail.com>
2022-11-09 13:23:08 +0100

    ejercicio final

--- src/main/java/Contraseña/TareaTema4.java
+++ src/main/java/Contraseña/TareaTema4.java
@@ -4,6 +4,8 @@
  */
 package Contraseña;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 import org.apache.commons.lang3.RandomStringUtils;
 
 /**
@@ -13,48 +15,73 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class TareaTema4 {
 
     public static void main(String[] args) {
-        //Opción 1 - Pin 4 dígitos;
 
-//        for (int i = 0; i < 10; i++) {
-//            String pin = RandomStringUtils.randomNumeric(4);
-//            System.out.println(pin);
-//        }
-//        
-//        System.out.println("--------------------");
-//        
-//         for (int i = 0; i < 10; i++) {
-//            String password = RandomStringUtils.randomAlphanumeric(8);
-//            System.out.println(password);
-//        }
-//         
-//         System.out.println("---------------------");
-//         
-//         //Array de caracteres o de tipo char;
-//         char[] conjuntoCaracteres = {'a', 'z', 'b', '$', '&', '1'};
-//         int numero = 5; 
-//         
-//         for (int i = 0; i < 10; i++) {
-//            String password2 = RandomStringUtils.random(numero, conjuntoCaracteres);
-//            System.out.println(password2);
-//        }
-//    
-//         System.out.println("----------------------");
-//         
-//        Hace una sucesio nde 15 numeros aleatorios del 0 al 9;
-//        System.out.println(RandomStringUtils.randomNumeric(15).toUpperCase());  
-        //Opción 1, crear numeros pin de teléfono;
-        for (int i = 0; i < 3; i++) {
-            String pin = RandomStringUtils.randomNumeric(4);
-            System.out.println(pin);
+        String menuPrograma, numPin, contraseña1, contraseña2;
+
+        do {
+            menuPrograma = menuPrincipal();
+            switch (menuPrograma) {
+                case "1" ->
+                    numPin = pinAleatorio();
+                case "2" ->
+                    contraseña1 = contraseñaAleatoria();
+                case "3" ->
+                    contraseña2 = contraseñaAleatoriaEligiendoDatos();
+            }
+        } while (!menuPrograma.equalsIgnoreCase("salir"));
+    }
+
+    //menu 
+    public static String menuPrincipal() {
+        String menu = JOptionPane.showInputDialog("""
+                                                  1. Pin aleatorio
+                                                  2. Contraseña aleatoria
+                                                  3. Elegir distancia de la contraseña
+                                                         """);
+        return menu;
+    }
+
+    //Opción 1, crear numeros pin de teléfono;
+    public static String pinAleatorio() {
+        String pin = "";
+        for (int i = 0; i < 1; i++) {
+            pin = RandomStringUtils.randomNumeric(4);
+            System.out.println("El pin creado es " + pin);
+            System.out.println("-------------------");
         }
+        return pin;
+    }
+    //Opción 2, crea contraseñas con números y letras aleatorias;
 
-        //Opción 2, crea contraseñas con números y letras aleatorias;
-        System.out.println("----------------------------");
+    public static String contraseñaAleatoria() {
+        String password = "";
 
-        for (int i = 0; i < 5; i++) {
-            String password = RandomStringUtils.randomAlphanumeric(i);
-            System.out.println(password);
+        for (int i = 0; i < 1; i++) {
+            password = RandomStringUtils.randomAlphanumeric(8);
+            System.out.println("La contraseña creada es " + password);
+            System.out.println("-------------------");
         }
+        return password;
+    }
+    //Opción 3, crear una contraseña con datos elegidos por mi y el usuario tiene que elegir la longitud 
+
+    public static String contraseñaAleatoriaEligiendoDatos() {
+        String password2 = "";
+
+        char[] conjuntoCaracteres = {'a', 'z', 'b', '$', '&', '1', 'c', '4', '9', 'D'};
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de caracteres que quiere que tenga tu contraseña: ");
+
+        int numero = teclado.nextInt();
+
+        for (int i = 0; i < 1; i++) {
+            password2 = RandomStringUtils.random(numero, conjuntoCaracteres);
+            System.out.println("La contraseña avanzada creada es " + password2);
+            System.out.println("-------------------");
+        }
+        return password2;
     }
 
 }
